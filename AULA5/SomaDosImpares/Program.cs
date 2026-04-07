5dca018729667b271ce7cd65543e20d7395b5825
joaolucaasz <joaolucasfernandescosta21@gmail.com>
2026-04-07 09:45:23 -0300

    Add files via upload

---
+++ AULA5/SomaDosImpares/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int soma = 0;
+
+        for (int numero = 50; numero <= 500; numero++)
+        {
+            if(numero % 3 == 0 && numero % 2 !=0)
+            {
+               soma += numero; 
+            }
+        }
+            Console.WriteLine("A soma dos número é: "+ soma);
+
+    }
+}
---
+++ AULA5/mediaAritmética/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int quantidade, contador = 0;
+        int numero, soma = 0, quantidadeDePares = 0;
+
+        Console.Write("Quantos números você vai digitar? ");
+        quantidade = int.Parse(Console.ReadLine());
+
+        while (contador < quantidade)
+        {
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0) 
+            {
+                soma += numero;
+                quantidadeDePares++;
+            }
+
+            contador++;
+        }
+
+        if (quantidadeDePares > 0)
+        {
+            double media = (double)soma / quantidadeDePares;
+            Console.WriteLine("Média dos pares: " + media);
+        }
+        else
+        {
+            Console.WriteLine("Nenhum número par foi digitado.");
+        }
+    }
+}
